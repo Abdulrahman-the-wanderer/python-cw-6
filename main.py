dbf58bf86626be075679c302097266d9ed535a5f
Abdulrahman ayman adi <142898159+Abdulrahman-the-wanderer@users.noreply.github.com>
2023-09-06 19:34:40 +0300

    my beautiful dictenory

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # write your code here
+person = {
+    "name" : "adi",
+    "age" : 16,
+    "hobbies" : ["reading","swimming","judo"]
+}
+print(person["name"])
+print(person["age"])
+person["age"] = 17
+person["country"] = "syria"
+print(len(person))
+person ["hobbies"].append("mma") 
+print(person)
+def check_hobbies(person) :
+    if len(person["hobbies"]) > 3 :
+        print("you are AMAZING")
+check_hobbies(person)
